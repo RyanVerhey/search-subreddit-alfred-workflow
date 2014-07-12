fa14067b8275967f61969f52b53141d7a044ff19
RyanVerhey <ryan90v@gmail.com>
2014-07-12 13:47:27 -0500

    Changed to make use of alfred_helper

--- search.rb
+++ search.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require_relative 'alfred_helper'
 
 query = ARGV.first
 
@@ -11,32 +12,31 @@ resp = Net::HTTP.get_response(query_url)
 resp = JSON.parse(resp.body)
 subs = resp["data"]["children"]
 
-xml = "<items>"
+xml = Alfred::Workflow.new
 
 if query == "reddit"
-  xml += "<item arg='#{default_url}' autocomplete='Reddit' valid='YES'>"
-  xml += "<title>Reddit homepage</title>"
-  xml += "<subtitle>#{default_url}</subtitle>"
-  xml += "<icon>icon.png</icon>"
-  xml += "</item>"
+  reddit = Alfred::Item.new({
+    arg: default_url,
+    autocomplete: 'Reddit',
+    title: 'Reddit homepage',
+    subtitle: default_url })
+  xml << reddit
 elsif subs.first == nil
-  xml += "<item valid='no'>"
-  xml += "<title>No Results. Keep Typing...</title>"
-  xml += "<subtitle>The reddit API is a bit weird. Keep typing and your subreddit should show up.</subtitle>"
-  xml += "<icon>icon.png</icon>"
-  xml += "</item>"
+  invalid = Alfred::Item.new({
+    valid: "no",
+    title: "No results yet. Keep Typing...",
+    subtitle: "The reddit API is a bit weird. Keep typing and your subreddit should show up." })
+  xml << invalid
 else
   subs.each do |sub|
     data = sub["data"]
-    xml += "<item arg='#{default_url + data["url"]}' autocomplete='#{data["display_name"]}' valid='YES'>"
-    xml += "<title>#{data['display_name']}#{" (private)" if data["subreddit_type"] == "private" }</title>"
-    # xml += "<subtitle>#{default_url + data["url"]}</subtitle>"
-    xml += "<subtitle>#{data["public_description"]}</subtitle>"
-    xml += "<icon>icon.png</icon>"
-    xml += "</item>"
+    subreddit = Alfred::Item.new({
+      arg: default_url + data["url"],
+      autocomplete: data["display_name"],
+      title: "#{data['display_name']}#{" (private)" if data["subreddit_type"] == "private" }",
+      subtitle: data["public_description"] })
+    xml << subreddit
   end
 end
-
-xml += "</items>"
 
 puts xml
